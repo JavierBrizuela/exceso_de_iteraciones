5ee7e54be91b3e491bedd292d8ffa00302f22d90
Elena Expósito <elexla97@gmail.com>
2024-06-02 23:15:58 +0200

    Add some basic structure and styles to Home

--- Frontend/src/routes/Home.jsx
+++ Frontend/src/routes/Home.jsx
@@ -1,5 +1,24 @@
+import "./Home.css";
+
 function Home() {
-  return <h2>Home</h2>;
+  return (
+    <section className="projects-home">
+      <div className="projects">
+        <h1>Descubre algunos de estos proyectos</h1>
+        <div className="project-list">
+          <h1>hola1</h1>
+          <h1>hola2</h1>
+          <h1>hola3</h1>
+        </div>
+      </div>
+      <div className="create-account">
+        <h2>¿Te gustaría unirte a algún proyecto?</h2>
+        <h2>¿O preferirías crear el tuyo propio?</h2>
+        <h3>¡Únete a la comunidad Develmatch!</h3>
+        <button>Crea tu cuenta gratis</button>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
