d33a49f9b211313b5d2b299c1b615476746f49d8
Elena Expósito <elexla97@gmail.com>
2024-06-03 18:35:03 +0200

    Add 'Create free account' section + WIP - make Card component reusable

--- Frontend/src/routes/Home/Home.jsx
+++ Frontend/src/routes/Home/Home.jsx
@@ -1,21 +1,65 @@
 import "./Home.css";
-import Card from "./components/Card";
+//import Card from "./components/Card";
+//import { useState, useEffect } from "react";
 
 function Home() {
+  /*const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProject = async (id) => {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/api/projects/${id}`);
+        const data = await response.json();
+        setProjects([data]);
+      } catch (error) {
+        console.error("Error fetching project data:", error);
+      }
+    };
+
+    const fetchFirstProjectId = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/projects/");
+        const data = await response.json();
+        if (data.length > 0) {
+          const firstProjectId = data[0].id;
+          fetchProject(firstProjectId);
+        }
+      } catch (error) {
+        console.error("Error fetching project list:", error);
+      }
+    };
+
+    fetchFirstProjectId();
+  }, []);*/
+
   return (
     <section className="projects-home">
       <div className="projects">
         <h1 className="projects-title">Descubre algunos de estos proyectos</h1>
         <div className="project-list">
-          <Card />
-          <Card />
+          {/*projects.map((project) => (
+            <Card
+              key={project.id}
+              id={project.id}
+              title={project.title}
+              type={project.type}
+              difficulty={project.difficulty}
+              languages={project.languages}
+              created_by={project.created_by}
+              actual_status={project.actual_status}
+            />
+          ))*/}
         </div>
       </div>
-      <div className="create-account">
-        <h2>¿Te gustaría unirte a algún proyecto?</h2>
-        <h2>¿O preferirías crear el tuyo propio?</h2>
-        <h3>¡Únete a la comunidad Develmatch!</h3>
-        <button>Crea tu cuenta gratis</button>
+      <div className="create-account-home">
+        <div className="account-home-text">
+          <div className="account-home-principal-text">
+            <h2>¿Te gustaría unirte a algún proyecto?</h2>
+            <h2>¿O preferirías crear el tuyo propio?</h2>
+          </div>
+          <p className="account-home-secondary-text">¡Únete a la comunidad Develmatch!</p>
+        </div>
+        <button className="account-home-button">Crea tu cuenta gratis</button>
       </div>
     </section>
   );
--- Frontend/src/routes/Home/components/Card.jsx
+++ Frontend/src/routes/Home/components/Card.jsx
@@ -1,32 +1,26 @@
 import "./Card.css";
+import PropTypes from "prop-types";
 
 function Card({ id, title, type, difficulty, languages, created_by, actual_status }) {
   return (
-    <div className="card-wrapper">
+    <div className="card-wrapper" id={id}>
       <div className="card-info">
-        <h3 className="card-title">Título del proyecto</h3>
+        <h3 className="card-title">{title}</h3>
         <div className="card-division-info">
           <div className="first-division-info">
-            <span className="tag project-type">Educación</span>
-            <span className="tag project-difficulty">Principiante</span>
+            <span className="tag project-type">{type}</span>
+            <span className="tag project-difficulty">{difficulty}</span>
           </div>
           <div className="second-division-info">
-            <span className="project-languages">JavaScript</span>
-            <span className="project-languages">Python</span>
-            <span className="project-languages">C</span>
-            <span className="project-languages">C++</span>
-            <span className="project-languages">Java</span>
-            <span className="project-languages">C#</span>
-            <span className="project-languages">SQL</span>
-            <span className="project-languages">Go</span>
-            <span className="project-languages">PHP</span>
-            <span className="project-languages">Visual Basic</span>
-            <span className="project-languages">Fortran</span>
-            <span className="project-languages">Otros</span>
+            {languages.map((language, index) => (
+              <span key={index} className="project-languages">
+                {language}
+              </span>
+            ))}
           </div>
           <div className="third-division-info">
-            <span className="tag project-actual-status">En progreso</span>
-            <span className="tag project-created-by">PepitoRubio</span>
+            <span className="tag project-actual-status">{actual_status}</span>
+            <span className="tag project-created-by">{created_by}</span>
           </div>
           <button className="card-more-info">+ Info</button>
         </div>
@@ -34,5 +28,15 @@ function Card({ id, title, type, difficulty, languages, created_by, actual_statu
     </div>
   );
 }
+
+Card.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  difficulty: PropTypes.string.isRequired,
+  languages: PropTypes.arrayOf(PropTypes.string).isRequired,
+  created_by: PropTypes.string.isRequired,
+  actual_status: PropTypes.string.isRequired,
+};
 
 export default Card;
