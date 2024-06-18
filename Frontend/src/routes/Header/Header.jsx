fd9087a5b86d6044e224dd507c888afc5d9e0f4e
Elena Expósito <elexla97@gmail.com>
2024-06-18 18:31:53 +0200

    Add header buttons to signup and signout

--- Frontend/src/routes/Header/Header.jsx
+++ Frontend/src/routes/Header/Header.jsx
@@ -3,11 +3,20 @@ import "./header.css";
 import logo from "../../assets/develmatch.png";
 import userImg from "../../assets/icons8-usename.png";
 import { AccessContext } from "../../App";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const { user } = useContext(AccessContext);
+  const { user, setAccess, setUser } = useContext(AccessContext);
+  const navigate = useNavigate();
   console.log(user);
+
+  const handleLogout = () => {
+    localStorage.removeItem("access");
+    localStorage.removeItem("refresh");
+    setAccess(null);
+    setUser(null);
+    navigate("/signin");
+  };
 
   return (
     <div className={"divHeader"}>
@@ -15,10 +24,21 @@ export default function Header() {
         <img className={"imgLogo"} src={logo} alt="Logo Develmatch" />
       </Link>
       <div className={"userNameDiv"}>
-        <p className={"userName"}>{user ? user.username : " "}</p>
         <div className={"divUserNameImg"}>
           <img className={"userNameImg"} src={userImg} alt="User Name image" />
         </div>
+        {user ? (
+          <>
+            <p className="userName">{user.username}</p>
+            <button className="logButton" onClick={handleLogout}>
+              Cerrar sesión
+            </button>
+          </>
+        ) : (
+          <Link to="/signin">
+            <button className="logButton">Iniciar sesión</button>
+          </Link>
+        )}
       </div>
     </div>
   );
