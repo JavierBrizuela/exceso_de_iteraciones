db22d1ff59f651f9e5b6a0e41d005b2cefd5851a
Elena Expósito <elexla97@gmail.com>
2024-06-18 14:01:34 +0200

    WIP - Header

--- Frontend/src/routes/Header/Header.jsx
+++ Frontend/src/routes/Header/Header.jsx
@@ -1,5 +1,7 @@
 import "./header.css";
 import logo from "../../assets/develmatch.png";
+import userImg from "../../assets/icons8-usename.png";
+
 export default function Header() {
   return (
     <div className={"divHeader"}>
@@ -8,6 +10,9 @@ export default function Header() {
       </div>
       <div className={"userNameDiv"}>
         <p className={"userName"}>User Name</p>
+        <div className={"divUserNameImg"}>
+          <img className={"userNameImg"} src={userImg} alt="User Name image" />
+        </div>
       </div>
     </div>
   );
--- Frontend/src/routes/Root.jsx
+++ Frontend/src/routes/Root.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 function Root() {
   return (
     <main>
-      <h1>Hello world!</h1>
       <Outlet />
     </main>
   );
